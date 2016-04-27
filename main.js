e07668d31e46b9256a8a095e640de3c65c02db8f
covega <john.morgan@live.com>
2016-04-27 13:52:18 -0700

    fixed files. added draggables

--- main.js
+++ main.js
@@ -33,58 +33,117 @@ var info = d3.select("body").append("div")
 var displayData = {};
 var data = {};
 
+
+
+var drag = d3.behavior.drag()
+	.on("drag", function(d, i){
+		d.x += d3.event.dx;
+		d.y += d3.event.dy;
+		d3.select(this).attr("transform", function(d, i){
+			return "translate(" + [ d.x,d.y ] + ")";
+		});
+		//get function 
+	});
+
+var pointOne = svg.append("circle")
+	.attr("class", '.point')
+	.attr("r", 10)
+	.data([{"x": 0, "y":0}])
+	.call(drag)
+
 var svg = svg.append('g')
 
+
+
+var pointTwo;
 var visualize = function(scpd_incidents){
-	var circle = svg.selectAll("circle")
-			.data(scpd_incidents.data)
-		.enter().append("circle")
-			.attr("class", "mark")
-			.attr("r", 2)			
-  			.attr("transform", function(d) {
-	    		return "translate(" + projection([
-	      			d.Location[0], //longitude
-	      			d.Location[1] //latitude
-	    		]) + ")";
-			})
-			.on("mouseover", label)
+	var marks = svg.selectAll(".mark")
+		.data(scpd_incidents, function(d){ return d.id; });
+	marks.enter().append("circle")
+		.attr("class", "mark")
+		.attr("r", 2)			
+			.attr("transform", function(d) {
+    		return "translate(" + projection([
+      			d.Location[0], //longitude
+      			d.Location[1] //latitude
+    		]) + ")";
+		})
+		.on("mouseover", label)
 			//.on("mouseout", function() {info.html(""); });
 			//TODO: uncomment for mouseout
 }
 
-var radiusA = 1000;
-var radiusB = 1000;
 
 var label = function(d){
 	info.html("Category: " + d.Category + "<br>Date: " + d.Date + "<br>Day: " + d.DayOfWeek + "<br>Location: " + d.Location);
 }
 
 //test
-var filterRange = function(A, B){
+var filterFromPoint = function(point, radius){
 	var toRemove = [];
-	for(var i = 0; i < display_data.length; i++){
-		if(d3.geo.distance(A, display_data[i]) < radiusA || d3.geo.distance(A, display_data[i]) < radiusB){
+	for(var i = 0; i < displayData.length; i++){
+		if(d3.geo.distance(point, displayData[i].Location) * 3959 > radius){
 			toRemove.push(i);
+			//console.log(displayData[i]);
+			//console.log(d3.geo.distance(point, displayData[i].Location) *3959888);
+		}	
+	}
+
+	for(var r = toRemove.length - 1; r >= 0; r--){
+		displayData.splice(toRemove[r], 1);
+	}
+
+	console.log(toRemove.length);
+	for(var x = 0; x < toRemove.length; x++){
+		for(var y = 0; y < displayData.length; y++){
+			if(data[toRemove[x]] === displayData[y]){
+				//console.log(d3.geo.distance(point, displayData[y].Location) * 3959);
+				console.log('np');
+			}
 		}
 	}
-	for(var r = 0; r < toRemove.length; r++){
-		display_data.splice(r, 1);
+
+	update();
+}
+
+var filterByAttr = function(attr, val){
+	var toRemove = [];
+	for(var i = 0; i < displayData.length; i++){
+		if(displayData[i][attr] == val){
+			toRemove.push(i);
+		}	
 	}
+
+	for(var r = toRemove.length - 1; r >= 0; r--){		
+		displayData.splice(toRemove[r], 1);
+	}	
+
+	update();	
 }
 
 var update = function(){
-	var circle = svg.selectAll("circle");
-	circle.exit().remove();
+	var marks = svg.selectAll(".mark").data(displayData);
+	marks.exit().remove();
 }
 
-d3.json('scpd_incidents.json', function(error, scpd_incidents){
-	if(error) throw error;
-	console.log(scpd_incidents);
-	data = scpd_incidents;
-	displayData = data;
-	visualize(displayData);
-});
 
+d3.json('scpd_incidents.json', function(error, scpd_incidents){
+	if(error) throw error;	
+	data = scpd_incidents.data;
+	//data.splice(10, data.length - 10);
+	for(var i = 0; i < data.length; i++){
+		data[i].id = '' + i;
+		//console.log(data[i].id);
+	}
+	displayData = data;
+	//console.log(displayData);	
+	visualize(displayData);	
+
+	var Categories = ['NON-CRIMINAL','LARCENY/THEFT','DRUG/NARCOTIC','VEHICLE THEFT','STOLEN TRUCK','BATTERY','BURGLARY','OTHER OFFENSES','ROBBERY','VANDALISM','PROBATION VIOLATION','ASSAULT','MISSING PERSON','FRAUD','STOLEN PROPERTY','WARRANTS','PROSTITUTION','WEAPON LAWS','LIQUOR LAWS','SUSPICIOUS OCC','SECONDARY CODES','SEX OFFENSES, FORCIBLE','SEX OFFENSES, NON FORCIBLE','DRUNKENNESS','TRESPASS','ARSON','DISORDERLY CONDUCT','KIDNAPPING','RUNAWAY','LOITERING','EMBEZZLEMENT','FORGERY/COUNTERFEITING','GAMBLING','DRIVING UNDER THE INFLUENCE','BRIBERY','SUICIDE','EXTORTION','FAMILY OFFENSES'];
+
+	//filterByAttr('Category', 'NON-CRIMINAL');
+//	filterFromPoint([-122.458220811697,37.7633123961354], 1);
+});
 
 
 
